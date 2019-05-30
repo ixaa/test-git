29429dcfe5cd12435d80cb098a2a1770eebae622
xanaja <xanaja@interia.pl>
2019-05-30 15:04:44 +0200

    first

---
+++ php72.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <title>Moja strona WWW</title>
+  </head>
+  <body>
+   <div>
+   <?php
+   $liczba1 = 2 * 5 + 18;
+   $liczba2 = $liczba1 / 2;
+   $liczba3 = $liczba2 - $liczba1;
+   $liczba4 = $liczba2 ** 2;
+
+   echo "Wartosc zmiennej \$liczba1 = $liczba1 <br>";
+   echo "Wartosc zmiennej \$liczba2 = $liczba2 <br>";
+   echo "Wartosc zmiennej \$liczba3 = $liczba3 <br>";
+   echo "Wartosc zmiennej \$liczba4 = $liczba4 <br>";
+
+   ?>
+   </div>
+  </body>
+</html>
